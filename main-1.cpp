b23be1e46083d35ae619ae264fda6a7a5638b34b
makssein <90086818+makssein@users.noreply.github.com>
2021-09-24 22:43:08 +0200

    Add files via upload

---
+++ main-1.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <fstream>
+using std::cout;
+using std::ifstream;
+using std::ofstream;
+using std::cin;
+using std::endl;
+#define N 100
+#define M 100
+
+int main() {
+    ifstream in("/Users/maksimsein/CLionProjects/untitled/input.txt");
+    ofstream out("/Users/maksimsein/CLionProjects/untitled/output.txt");
+    int matrix[N][M];
+    int n,m;
+    int sum, a, b;
+    bool check;
+    sum=0;
+    in >> n >> m;
+    for (int i=0; i<n; i++) {
+        for (int j = 0; j < m; j++) {
+            in >> matrix[i][j];
+        }
+    }
+    for (int i=0; i<n; i++){
+        for (int j=0; j<m; j++){
+            b = matrix[i][j];
+            while (b > 0){
+                a = b%10;
+                sum+=a;
+                b=b/10;
+            }
+            if(sum==14) {
+                check = true;
+                break;
+            }
+            sum=0;
+        }
+        if(check) break;
+    }
+    if(check){
+        for (int k = 0; k < n; k++)
+            for (int p = 0; p < m; p++)
+                for (int i = 0; i < n; i++)
+                    for (int j = 0; j < m; j++)
+                        if (matrix[i][j] > matrix[k][p])
+                            std::swap (matrix[i][j],matrix[k][p]);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                out << matrix[i][j] << " ";
+            }
+            out << endl;
+        }
+    }
+    else {
+        out << "Необходимое условие не выявлено";
+    }
+
+
+    return 0;
+}
